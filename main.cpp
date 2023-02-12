c45e4aa85bc9b98bcdbb587f74fe4b8cc6d1fe57
kenjinote <2605401+kenjinote@users.noreply.github.com>
2023-02-12 12:30:36 +0900

    initial commit

---
+++ main.cpp
@@ -0,0 +1,73 @@
+#define UNICODE
+#pragma comment(linker,"/opt:nowin98")
+#include<windows.h>
+#include"resource.h"
+
+TCHAR szClassName[]=TEXT("Window");
+
+LRESULT CALLBACK WndProc(HWND hWnd,UINT msg,WPARAM wParam,LPARAM lParam)
+{
+	switch(msg)
+	{
+	case WM_COMMAND: // メニューの処理はここで行う
+		switch(LOWORD(wParam))
+		{
+		case ID_END:
+			SendMessage(hWnd,WM_CLOSE,0,0);
+			break;
+		}
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd,msg,wParam,lParam);
+	}
+	return 0;
+}
+
+EXTERN_C void __cdecl WinMainCRTStartup()
+{
+	MSG msg;
+	HINSTANCE hInstance=GetModuleHandle(0);
+	WNDCLASS wndclass=
+	{
+		CS_HREDRAW|CS_VREDRAW,
+		WndProc,
+		0,
+		0,
+		hInstance,
+		0,
+		LoadCursor(0,IDC_ARROW),
+		(HBRUSH)(COLOR_WINDOW+1),
+		MAKEINTRESOURCE(IDR_MENU1), // ここでメニューを設定
+		szClassName
+	};
+	RegisterClass(&wndclass);
+	HWND hWnd=CreateWindow(
+			szClassName,
+			TEXT("Window"),
+			WS_OVERLAPPEDWINDOW,
+			CW_USEDEFAULT,
+			0,
+			CW_USEDEFAULT,
+			0,
+			0,
+			0, // ここでもメニューを設定できるがクラスメニューを使う場合は0でOK
+			hInstance,
+			0
+		);
+	ShowWindow(hWnd,SW_SHOWDEFAULT);
+	UpdateWindow(hWnd);
+	while(GetMessage(&msg,0,0,0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	ExitProcess(msg.wParam);
+}
+
+#if _DEBUG
+void main(){}
+#endif
+
